b0560ed0e9b3c1d1389f9de3fd2484b39a23e6c9
Rishi-Bidani <rishibidani12@gmail.com>
2021-04-06 01:55:11 +0530

    Added navigation to different HTML files

--- main.js
+++ main.js
@@ -4,22 +4,52 @@ const path = require("path");
 const { Menu } = require("electron/main");
 const { app, BrowserWindow, ipcMain, webContents } = electron;
 
-app.on("ready", function () {
-  mainWindow = new BrowserWindow({
-    width: 1100,
-    minWidth: 900,
-    minHeight: 700,
-  });
-  mainWindow.maximize();
-  mainWindow.loadURL(
-    url.format({
-      pathname: path.join(__dirname, "templates/home.html"),
-      protocol: "file:",
-      slashes: true,
-    })
-  );
-  //Quit app when closed
-  mainWindow.on("closed", function () {
-    app.quit();
-  });
+let windows;
+
+app.on("ready", function() {
+    windows = new BrowserWindow({
+        width: 1100,
+        minWidth: 900,
+        minHeight: 700,
+        // frame: false,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+        },
+    });
+    windows.maximize();
+    windows.loadURL(
+        url.format({
+            pathname: path.join(__dirname, "templates/home.html"),
+            protocol: "file:",
+            slashes: true,
+        })
+    );
+    //Quit app when closed
+    windows.on("closed", function() {
+        app.quit();
+    });
 });
+
+ipcMain.on("which:Window", (e, item) => {
+    if (item == "window:home") {
+        windows.loadURL(
+            url.format({
+                pathname: path.join(__dirname, "templates/home.html"),
+                protocol: "file:",
+                slashes: true,
+            })
+        );
+
+    } else if (item == "window:PlanBudget") {
+        windows.loadURL(
+            url.format({
+                pathname: path.join(__dirname, "templates/planBudget.html"),
+                protocol: "file:",
+                slashes: true,
+            })
+        );
+
+    }
+})
